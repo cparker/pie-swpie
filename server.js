604b724fd7c8e8e685e1ff989cabf353a4ec684f
Christian Parker <christian.parker@emc.com>
2018-03-21 12:02:32 -0600

    added tally for up/down voting

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const dbURI = process.env.MONGODB_URI || defaultDBConnection
 console.log(`dbURI ${dbURI}`)
 const MongoClient = require('mongodb').MongoClient;
 const cookieName = 'pieSwipeID'
+const defaultAppName = 'pie-swipe'
 let db
 
 app.use(cookieParser())
@@ -43,6 +44,80 @@ app.use((req, res, next) => {
     next()
 })
 
+/*
+  given a collection name, and a url 
+    move a vote from up to down if the vote is 1, 
+    move a vote from down to up of the vote is -1
+*/
+async function changeVote(imageColName, url, vote) {
+    const voteChangeUpdate = vote === 1 ? {
+        "$inc": {
+            totalUp: 1,
+            totalDown: -1
+        }
+    } : {
+        "$inc": {
+            totalUp: -1,
+            totalDown: 1
+        }
+    }
+
+    await db.collection(imageColName)
+        .update({
+            url: url
+        }, voteChangeUpdate)
+}
+
+/*
+  given the name of the image collection, a url, and a vote (1 | -1)
+  tally the totalUp, totalDown for the url
+
+*/
+async function tallyVote(imageColName, url, vote) {
+
+    const imageRecord = await db.collection(imageColName)
+        .findOne({
+            url: url
+        })
+
+    if (imageRecord) {
+        // we have a record of this image already, simply tally the vote
+        const upVote = {
+            "$inc": {
+                totalUp: 1
+            }
+        }
+
+        const downVote = {
+            "$inc": {
+                totalDown: 1
+            }
+        }
+
+        const voteUpdate = vote === 1 ? upVote : downVote
+
+        await db.collection(imageColName)
+            .updateOne({
+                url: url
+            }, voteUpdate)
+    } else {
+        // first time anyone is voting for this image
+        const initialUpVote = {
+            url: url,
+            totalUp: 1,
+            totalDown: 0
+        }
+        const initialDownVote = {
+            url: url,
+            totalUp: 0,
+            totalDown: 1
+        }
+        const initialVote = vote === 1 ? initialUpVote : initialDownVote
+        await db.collection(imageColName)
+            .insertOne(initialVote)
+    }
+}
+
 // handle POST of /swipe
 /*
   we have a cookieID and an image url, and we want to record an up or down vote
@@ -50,7 +125,7 @@ app.use((req, res, next) => {
   we'd like to quickly access the total votes by key
 
   image collection : { url, totalUp, totalDown} // one entry per image, pretty big
-  user vote collection : { cookieId, url, vote} // one entry per user, per image , really BIG
+  user vote collection : { clientId, url, vote} // one entry per user, per image , really BIG
 
   if user hasnt voted for url
     add entry to user vote collection
@@ -63,10 +138,68 @@ app.use((req, res, next) => {
     else
       change vote in user vote collection
       change count in image collection
+
+      JSON body looks like 
+      {
+          "appName" : "", // name of the app, like pieSwipe, or kitten, or , ....
+          "url" : "....",
+          "clientId" : "....", // the cookie
+          "vote" : 1 // 1 | -1
+      }
 */
-app.post('/swipe', (req, res, next) => {
+app.post('/swipe', async (req, res, next) => {
     console.log('body', req.body)
+
+    // prepend the appName to the col name
+    const userVoteColName = `${req.body.appName}-userVotes`
+    const imageColName = `${req.body.appName}-images`
+    const clientId = req.cookies[cookieName]
+
+    const userVoteRecord = await db.collection(userVoteColName)
+        .findOne({
+            clientId: clientId,
+            url: req.body.url
+        })
+
+    if (!userVoteRecord) {
+        // user has not voted yet
+
+        // record the user's vote in the userVote collection
+        await db.collection(userVoteColName)
+            .insertOne({
+                clientId: clientId,
+                url: req.body.url,
+                vote: req.body.vote
+            })
+
+        // then update the total votes for the image
+        await tallyVote(imageColName, req.body.url, req.body.vote)
+    } else {
+        console.log(`user ${clientId} has already voted for ${req.body.url}`)
+        // user has already voted
+        if (req.body.vote !== userVoteRecord.vote) {
+            console.log(`user ${clientId} is changing their vote on ${req.body.url} from ${userVoteRecord.vote} to ${req.body.vote}`)
+            // only need to do work if they are changing their vote
+            await db.collection(userVoteColName)
+                .updateOne({
+                    clientId: clientId,
+                    url: req.body.url
+                }, {
+                    "$set": {
+                        vote: req.body.vote
+                    }
+                })
+
+            // they changed their vote, so we need to move 1 vote from up to down, or vice versa
+            await changeVote(imageColName, req.body.url, req.body.vote)
+        }
+    }
+
+    console.log('got userVoteRecord', userVoteRecord)
+
     res.sendStatus(200)
+
+
 })
 
 
